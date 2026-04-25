84e245caddc5121fc9c95acfb4efa3cb6cc4c643
Taimur Khan <124710099+taimur55@users.noreply.github.com>
2026-04-25 21:45:05 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import P3Menu from './P3Menu.jsx'
+import App from './App.jsx' // Let App.jsx handle the layout
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
