62ca6d12a907c966f7e33a90c70f117eb7c97cae
Taimur Khan <124710099+taimur55@users.noreply.github.com>
2026-04-25 19:59:28 +0100

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import P3Menu from './P3Menu.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <P3Menu />
+  </React.StrictMode>,
+)
